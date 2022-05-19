1b2aebe0127dd6ad4233f45c3a8d143f09b6513d
Marco Ambrosio <marco.ambrosio@polito.it>
2022-05-19 20:54:47 +0200

    correction on bash script

--- utils/start_SR_stream.bash
+++ utils/start_SR_stream.bash
@@ -1,35 +1,31 @@
 #!/bin/bash
-while getopts e:h:w:i: flag
-do
-    case "${flag}" in
-        e) encoding=${OPTARG};;
-        h) height=${OPTARG};;
-        w) width=${OPTARG};;
-        i) ipaddr=${OPTARG};;
-    esac
-done
+
+# Example use
+# bash start_SR_stream.bash JPEG 80 60 127.0.0.1
+
+encoding=$1
+width=$2
+height=$3
+ipaddr=$4
+
 
 echo "encoding: ${encoding}"
-echo "height: ${height}"
-echo "width: ${width}"
+echo "frame: ${width}X${height}"
 echo "sending to: ${ipaddr}"
 
-if test $encoding='JPEG'
-then
+if [ $encoding = 'JPEG' ]; then
+echo "Streaming JPEG"
 gst-launch-1.0 v4l2src  device=/dev/video0 ! video/x-raw,width=640,height=480,framerate=15/1 \
 ! queue ! videoconvert ! videoscale ! video/x-raw,width=$width,height=$height ! videoconvert \
 ! jpegenc ! rtpjpegpay \
 ! queue leaky=2 ! udpsink host="${ipaddr}" port=5000
 fi
 
-if test $encoding='H264'
-then
+if [ $encoding = 'H264' ]; then
+echo "Streaming H264"
 gst-launch-1.0 v4l2src  device=/dev/video0 ! video/x-raw,width=640,height=480,framerate=15/1 \
 ! queue ! videoconvert ! videoscale ! video/x-raw,width=$width,height=$height ! videoconvert \
 ! x264enc speed-preset=superfast tune=zerolatency ! rtph264pay \
 ! queue leaky=2 ! udpsink host="${ipaddr}" port=5000
 fi
-
-
-
 
